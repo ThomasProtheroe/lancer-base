2a6be39b7745ace904c4ac493770942d5e137df2
Thomas Protheroe <CanadianCavalry@gmail.com>
2024-03-18 16:22:31 -0700

    Added modals for addon purchases

--- app.js
+++ app.js
@@ -2,9 +2,21 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-//Main application route
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//Main application routes
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname,"/pages/base.html"));
+});
+app.post('/update/base', function (req, res) {
+    console.log('update request');
+    console.log(req);
+    updateBase();
+
+    res.send({
+        'status': 'success',
+    });
 });
 
 //Resource routes (images, data objects etc)
@@ -12,10 +24,16 @@ app.get('/images/:filename', function (req, res) {
     res.sendFile(path.join(__dirname,"/images/" + req.params['filename']));
 });
 app.get('/data/:filename', function (req, res) {
-    console.log('json request');
     res.sendFile(path.join(__dirname,"/data/" + req.params['filename']));
 });
+
 
 app.listen(9000, function () {
     console.log('LancerBase listening on port 9000');
 });
+
+
+
+const updateBase = () => {
+    return;
+}
