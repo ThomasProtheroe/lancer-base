2d06f6c04d9a7baa5e28758acc894ff2297a0723
Thomas Protheroe <CanadianCavalry@gmail.com>
2024-03-20 12:28:51 -0700

    Cache data files

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var fs = require('fs');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+let baseData = {};
+baseData = require('./data/base.json');
 
 //Main application routes
 app.get('/', function (req, res) {
@@ -11,7 +15,6 @@ app.get('/', function (req, res) {
 });
 app.post('/update/base', function (req, res) {
     console.log('update request');
-    console.log(req);
     updateBase();
 
     res.send({
@@ -20,6 +23,10 @@ app.post('/update/base', function (req, res) {
 });
 
 //Resource routes (styles, images, data objects etc)
+app.get('/data/baseData', function(req, res) {
+    console.log(baseData);
+    res.send(baseData);
+});
 app.get('/styles.css', function (req, res) {
     res.sendFile(path.join(__dirname,"/styles.css"));
 });
