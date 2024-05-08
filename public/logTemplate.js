be5ebbd3c666bfc4c11c7e2fdb9a10ed583df57d
LadyElle <elle.stafford@gmail.com>
2024-05-07 22:47:53 -0700

    9-the-logs-view-needs-to-auto-scroll-to-latest-message (#10)
    * 9 - Changed the scroll top to the scroll height of the log window to go to the bottom when a new message is added
    
    * 9 - Created new log template file to adjust the log in one place
    
    Removed the log from each place, so one object is now used; added checks for GM and Zen, so their versions are specific to them; added column limit to prevent new resources from continuing on in a row

---
+++ public/logTemplate.js
@@ -0,0 +1,64 @@
+function createLogTemplate(selectedPilot) {
+	if (!selectedPilot) return;
+
+	var logTemplate = `
+	<div id="logTemplate" class="row logs-header">
+		<h2 class="addon-header">Game Logs</h2>
+	</div>
+	<div class="row">
+		<div id="log-display" class="row">
+			{{#each logs}}
+				<div>
+					<span class="{{user}}_text">[{{ user }}]</span> - {{ message }}
+				</div>
+			{{/each}}
+		</div>
+	</div>
+	<div id="log-input" class="row">
+		<div>
+			<span class="${selectedPilot}_text">${selectedPilot}</span>
+			<span>:~$ </span>
+		</div>
+		<div>
+			<input id="log-text-input" type="text"/>
+		</div>
+	</div>`;
+
+	return logTemplate;
+}
+
+async function sendMessage(e) {
+	if (e.code == "Enter" || e.code == "NumpadEnter"){
+		const message = {
+			user: parent.state.selectedPilot,
+			time: Date.now(),
+			message: this.value
+		};
+		await fetch('./log', {
+			method: 'POST',
+			body: JSON.stringify(message),
+			headers: {
+				"Content-type": "application/json; charset=UTF-8"
+			}
+		});
+	}
+}
+
+function addLog(log) {
+	this.state.logs.push(log);
+	renderLogs();
+}
+
+function renderLogs() {
+	const logs = this.state.logs;
+	const selectedPilot = this.state.selectedPilot;
+	const source = createLogTemplate(selectedPilot);
+	const template = Handlebars.compile(source);
+	const rendered = template({ logs, selectedPilot});
+	const logContainer = document.getElementById('log-container');
+	logContainer.innerHTML = rendered;
+	logContainer.scrollTop = logContainer.scrollHeight;
+	document.getElementById('log-container').innerHTML = rendered;
+	const logInput = document.getElementById("log-text-input");
+	logInput.onkeydown = sendMessage;
+}
